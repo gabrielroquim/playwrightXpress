13edb9a19d506e66ac70da0f7f8521068385eb5e
qa <qa@local>
2026-04-26 10:05:30 -0300

    correçao banco de dados

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,9 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 
-test( 'deve poder cadastrar uma nova tarefa', async ({ page }) => {
+test('deve poder cadastrar uma nova tarefa', async ({ page }) => {
   await page.goto('http://localhost:8080');
-  await page.fill('input[class*=listInputNewTask]', 'Ler um livro de qualidade de software')
-// DICA monstar localizador por classe input[class*=listInputNewTask]
+
+  const inputTaskName = page.locator('input[class*=InputNewTask]')
+  await inputTaskName.fill('Ler um livro de qualidade de software')
+
+  await page.click('css=button >> text=Create') /// em vez de usar xpath, podemos usar o seletor de texto do Playwright para clicar no botão "Create"
+ // await page.click('//button[contains(text(),"Create")]')
 
 })
