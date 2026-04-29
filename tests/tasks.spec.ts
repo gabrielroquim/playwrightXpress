e751e474369cdac15d9ceb54d7e488a24032ab7a
qa <qa@local>
2026-04-29 15:46:07 -0300

    add rota relativa urlbase

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -60,7 +60,7 @@ test.describe('atulização de tarefas', () => {
 })
 
 test.describe('delete de tarefas', () => {
-  test.only('deve deletar uma tarefa', async ({ page, request }) => {
+  test('deve deletar uma tarefa', async ({ page, request }) => {
     const task = data.delete as TaskModel
 
     await deleteTaskByHelper(request, task.name)
